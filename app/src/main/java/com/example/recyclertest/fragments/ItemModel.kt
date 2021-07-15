8d3ed9719c9d5f3aba03a15bf77c250e14c957d4
Skvortsov <a.p.skvortsov@voodoo.rocks>
2021-07-15 19:02:43 +0200

    Add id for ItemModel

--- app/src/main/java/com/example/recyclertest/fragments/ItemModel.kt
+++ app/src/main/java/com/example/recyclertest/fragments/ItemModel.kt
@@ -1,10 +1,11 @@
 package com.example.recyclertest.fragments
 
-import java.util.Date
+import java.util.*
 
 data class ItemModel(
     val title: String,
     val description: String,
     val date: Date,
     var isFavorite: Boolean,
+    val id: String = UUID.randomUUID().toString(),
 )
